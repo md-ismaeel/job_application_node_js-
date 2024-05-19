af89087caceb0da504a5c90e9bca9dbcfd88c645
Md Ismail <mdismaeelkhan345@gmail.com>
2024-05-19 23:02:37 +0530

    updated live server

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 
 
 // Db Connecting
-mongoose.connect('mongodb://localhost:27017/jobApp')
+mongoose.connect('mongodb+srv://ismail:ismaildb@jobapp.qqzbkxc.mongodb.net/')
     .then(() => console.log('Connection with Database established successfully'))
     .catch((err) => console.log(`ERROR CONNECTING WITH DATABASE ${err}`))
 
